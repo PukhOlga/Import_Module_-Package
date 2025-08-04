923c6e0ac7d59a9e01d8d589592727cfd70a8638
PukhOlga <olga_pukh@vk.com>
2025-08-04 21:59:21 +0300

    Add files via upload

---
+++ scripts/dirty_main.py
@@ -0,0 +1,5 @@
+from Application.DB.people import get_employees
+from Application.salary import calculate_salary
+from search_in_json import search_in_json
+import datetime
+from pprint import pprint
